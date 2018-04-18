ed7230ca0d3f3c5dc382f1553b5f904b0d5c80f8
Sudhin Kumar Biswas <sudhinbiswas@hotmail.com>
2018-04-18 20:10:02 +0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,20 +1,28 @@
-## Getting full dataset
-data_full <- read.csv("./Data/household_power_consumption.txt", 
-                      header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, 
-                      stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+## read whole data
+whole_data <- read.csv("./Data/household_power_consumption.txt", 
+                       sep=';', stringsAsFactors=F, na.strings="?", nrows=2075259, header=T,
+                       check.names=F, comment.char="", quote='\"')
+
+## convert the Date variable to Date classes
+whole_data$Date <- as.Date(whole_data$Date, format="%d/%m/%Y")
 
 ## Subsetting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
+sub_data <- subset(whole_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
+## Delete the previous raw dataset
+rm(whole_data)
 
-## Plot 2
-plot(data$Global_active_power~data$Datetime, type="l",
+## Converting dates and add time data to data_time variable 
+date_time <- paste(as.Date(sub_data$Date), sub_data$Time)
+
+## Create new variable name as Datetime and put date_time data to sub_data dataset under Datetime variable 
+sub_data$Datetime <- as.POSIXct(date_time)
+
+## creating Plot-2 with transparent Background
+par(bg = "transparent")
+plot(sub_data$Global_active_power~sub_data$Datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
+
+## Saving plot-2 to png file
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
